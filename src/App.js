86bc2c7c771d88590ebf7e692e9281424f9975c1
Junior Fernandes <juniorbass8@gmail.com>
2019-07-31 11:50:58 -0300

    finish timer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import Square from './componentes/square';
-import Button from './componentes/button';
+import Timer from './componentes/timer';
 
 
 class App extends Component {
-
   constructor () {
-    console.log('constructor')
     super()
     this.state = {
-      color: 'red'
+      showTimer:true
     }
   }
 
-  componentWillMount () {
-    console.log('componentWillMount')
-  }
-
-  componentDidMount () {
-    console.log('componentDidMount')
-  }
-  
   render() {
     console.log('render')
     return(
       <div>
-        <Square color={this.state.color} />
-
-        {['green', 'red', 'purple', 'black', 'gold', 'gray'].map((color, index) => (
-          <Button key={index} onClick={() => this.setState({ color })}>{color}</Button>
-        ))}
-        
+        {this.state.showTimer && <Timer />}
+        <button onClick={() => this.setState({showTimer: !this.state.showTimer})}>Show hide/timer</button>
       </div>
     )
   }
--- src/componentes/timer/index.js
+++ src/componentes/timer/index.js
@@ -4,14 +4,26 @@ class Timer extends Component {
     constructor () {
         super()
         this.state = {
-            timer: 0
+            time: 0
         }
     }
 
+    //executa como o componente é montado
+    componentDidMount () {
+        this.timer = setInterval(() => this.setState({ time: this.state.time + 1}), 1000)
+    }
+
+    //executa quando queremos desmonstar o componente
+    componentWillUnmount () {
+        clearInterval(this.timer)
+    }
     //metodo render
     render () {
-        <div>Timer: {this.state.timer}</div>
+        return(
+            <div>Timer: {this.state.time}</div>
+        )
     }
+
 }
 
 export default Timer
